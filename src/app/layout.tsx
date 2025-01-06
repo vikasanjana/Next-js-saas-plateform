a50aa221f7e2992f7253332647c95e2ab9516174
vikasanjana <va6903@gmail.com>
2025-01-06 22:55:55 +0530

    Install taiwind requirement, Edit tailwind config and add Sadcn library theme variables into the css file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "../lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          'min-h-screen font-sans antialiased grainy', 
+          `${geistSans.variable} ${geistMono.variable}`
+        )}
       >
         {children}
       </body>
